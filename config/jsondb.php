5c7df0e8e691c22e37fe7bcf8e6bf501b2d0795f
mnavarrocarter <mnavarrocarter@gmail.com>
2017-02-21 17:11:09 -0300

    1.0.1 Fixes

--- config/jsondb.php
+++ config/jsondb.php
@@ -1,15 +1,20 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
 /*
 | -------------------------------------------------------------------
 | JSONDB CONFIG
 | -------------------------------------------------------------------
 | This file will contain some jsondb' settings.
 |
-| $config['jsondb_url']		The url of the json file
+| $config['jsondb_dir']			The directory where json files are located
+| $config['jsondb_default']	The name of the default db file (for restore)
+| $config['jsondb_active']	The name of the active db file
 |
 */
 
-$config['jsondb_url'] = 'app/data/data.json';
+$config['jsondb_dir'] = 'app/data/';
+$config['jsondb_default'] = 'default.json';
+$config['jsondb_active'] = 'data.json';
 
 
 /* End of file jsondb.php */
--- libraries/Jsondb.php
+++ libraries/Jsondb.php
@@ -1,14 +1,16 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-###################################################
-# JSONDB: Una micro base de datos en formato json #
-###################################################
-#                                                 #
-# Autor: Matías Navarro Carter                    #
-#                                                 #
-# Licencia: MIT                                   #
-#                                                 #
-###################################################
+/*
+| -------------------------------------------------------------------
+| JSONDB LIBRARY
+| -------------------------------------------------------------------
+| This file contains the JsonDB main functions.
+|
+| Developed by: Matías Navarro Carter
+| Version: 1.0.0
+| License: MIT
+|
+*/
 
 class Jsondb {
 
@@ -23,42 +25,58 @@ class Jsondb {
 		// Load config file
 		$this->ci->load->config('jsondb');
 		// Get options
-		$this->jsondb_url = $this->ci->config->item('jsondb_url');
+		$this->jsondb_dir = $this->ci->config->item('jsondb_dir');
+		$this->jsondb_default = $this->ci->config->item('jsondb_default');
+		$this->jsondb_active = $this->ci->config->item('jsondb_active');
 		
 		log_message('debug', "Jsondb Class Initialized");
 	}
 
-	 /**
-     * Decodes the json and returns it as an object. If $array is set to true, returns an array. 
-     *
-     * @param boolean $array
-     * @return array or object
-     * @access protected 
-     */
+	/**
+	  * Decodes the json and returns it as an object. If $array is set to true, returns an array. 
+	  *
+	  * @param boolean $array
+	  * @return array or object
+	  * @access protected 
+	  */
 	protected function decode($array = false) {
-		$json = file_get_contents($this->jsondb_url,0,null,null);
+		$file = $this->jsondb_dir.$this->jsondb_active;
+		$json = file_get_contents($file,0,null,null);
 		$json_output = json_decode($json, $array);
 		return $json_output;
 	}
 
-	 /**
-     * Encodes the json from an array and saves it in the json file. 
-     *
-     * @param array $data
-     * @access protected 
-     */
+	/**
+      * Encodes the json from an array and saves it in the json file. 
+      *
+      * @param array $data
+      * @access protected 
+      */
 	protected function encode($data) {
+		$file = $this->jsondb_dir.$this->jsondb_active;
 		$jsonData = json_encode($data, JSON_PRETTY_PRINT);
-		file_put_contents($this->jsondb_url, $jsonData);
+		file_put_contents($file, $jsonData);
 		return;
 	}
 
 	/**
-     * Creates a new table.
-     *
-     * @param string $name
-     * @access public 
-     */
+      * Restores the database file to the default. 
+      *
+      * @access public 
+      */
+	public function restore() {
+		$file = $this->jsondb_dir.$this->jsondb_active;
+		$backup = $file = $this->jsondb_dir.$this->jsondb_default;
+		file_put_contents($file, $backup);
+		return;
+	}
+
+	/**
+      * Creates a new table.
+      *
+      * @param string $name
+      * @access public 
+      */
 	public function create_table($name) {
 		$query = $this->decode(true);
 		if ($query[$name]) {
@@ -76,11 +94,11 @@ class Jsondb {
 	}
 
 	/**
-     * Deletes a table.
-     *
-     * @param string $name
-     * @access public 
-     */
+      * Deletes a table.
+      *
+      * @param string $name
+      * @access public 
+      */
 	public function drop_table($name) {
 		$query = $this->decode(true);
 		if ($query[$name]) {
@@ -93,11 +111,11 @@ class Jsondb {
 	}
 
 	/**
-     * Empties a table.
-     *
-     * @param string $name
-     * @access public 
-     */
+      * Empties a table.
+      *
+      * @param string $name
+      * @access public 
+      */
 	public function empty_table($name) {
 		$query = $this->decode(true);
 		$save_id = $query[$name]['_metadata']['id'];
@@ -117,25 +135,25 @@ class Jsondb {
 	}
 
 	/**
-     * Gets all the data from the database as an object 
-     *
-     * @return object
-     * @access public 
-     */
+      * Gets all the data from the database as an object 
+      *
+      * @return object
+      * @access public 
+      */
 	public function get_all() {
 		$query = $this->decode();
 		return $query;
 	}
 
 	/**
-     * Gets one whole record from the specified $table, whose $field matches the $value sended.
-     *
-     * @param string $table
-     * @param string $field
-     * @param string $value
-     * @return object
-     * @throws Error if field or table are not found (TODO)
-     */
+      * Gets one whole record from the specified $table, whose $field matches the $value sended.
+      *
+      * @param string $table
+      * @param string $field
+      * @param string $value
+      * @return object
+      * @throws False if field or table are not found
+      */
 	public function get_record($table, $field, $value) {
 		$query = $this->decode();
 		if (!$query->$table) {
@@ -150,16 +168,16 @@ class Jsondb {
 		}
 	}
 
-	 /**
-     * Gets all data from a $table. Also an specific item (or items) if optional params are present
-     * Then returns the data sorted by field if optional params are present
-     *
-     * @param string $table
-     * @param string $field (optional)
-     * @param string $value (optional)
-     * @return array
-     * @throws Error if field or table are not found (TODO)
-     */
+	/**
+      * Gets all data from a $table. Also an specific item (or items) if optional params are present
+      * Then returns the data sorted by field if optional params are present
+      *
+      * @param string $table
+      * @param string $field (optional)
+      * @param string $value (optional)
+      * @return array
+      * @throws Error if field or table are not found
+      */
 	public function get_records($table, $field = false, $value = false, $sortfield = false) {
 		$query = $this->decode(true);
 		$result = array();
@@ -188,14 +206,14 @@ class Jsondb {
 	}
 
 	/**
-     * Updates the entry on a $table with $array, whose $field matches the $value sended.
-     *
-     * @param string $table
-     * @param string $field
-     * @param string $value
-     * @param array $array
-     * @throws Error if field or table are not found (TODO)
-     */
+      * Updates the entry on a $table with $array, whose $field matches the $value sended.
+      *
+      * @param string $table
+      * @param string $field
+      * @param string $value
+      * @param array $array
+      * @throws Error if field or table are not found
+      */
 	public function update_record($table, $field, $value, $array) {
 		$query = $this->decode(true);
 		if (!$query[$table]) {
@@ -215,13 +233,13 @@ class Jsondb {
 	}
 
 	/**
-     * Deletes an record from $table whose $field matches the $value sended.
-     *
-     * @param string $table
-     * @param string $field
-     * @param string $value
-     * @throws Error if field or table are not found (TODO)
-     */
+      * Deletes an record from $table whose $field matches the $value sended.
+      *
+      * @param string $table
+      * @param string $field
+      * @param string $value
+      * @throws Error if field or table are not found
+      */
 	public function delete_record($table, $field, $value) {
 		$query = $this->decode(true);
 		if (!$query[$table]) {
@@ -242,13 +260,13 @@ class Jsondb {
 	}
 
 	/**
-     * Creates an record on $table with $data array. Then returns the unique id of that created item.
-     *
-     * @param string $table
-     * @param array $data
-     * @return string 'Unique id'
-     * @throws Error if field or table are not found (TODO)
-     */
+      * Creates an record on $table with $data array. Then returns the unique id of that created item.
+      *
+      * @param string $table
+      * @param array $data
+      * @return string 'Unique id'
+      * @throws Error if field or table are not found
+      */
 	public function insert_record($table, $data) {
 		$query = $this->decode(true);
 		if ($query[$table]) {
@@ -267,3 +285,6 @@ class Jsondb {
 	}
 
 }
+
+/* End of file Jsondb.php */
+/* Location: ./application/config/Jsondb.php */
